eef9b12602f09886d80798e96241e64b777b6e4e
itoayano25 <anoori1925@gmail.com>
2024-03-17 11:56:41 +0900

    マルチログイン部分誤字修正

--- influencer_education/app/Exceptions/Handler.php
+++ influencer_education/app/Exceptions/Handler.php
@@ -52,7 +52,7 @@ class Handler extends ExceptionHandler
     protected function unauthenticated($request,Throwable $exception)
     {
         if($request->expectsJson()) {
-            return respomse()->json(['message' => $exception->getMessage()],401);
+            return response()->json(['message' => $exception->getMessage()],401);
         }
         if($request->is('admin') || $request->is('admin/*')){
             return redirect()->guest('/admin/login');
--- influencer_education/app/Models/Admin.php
+++ influencer_education/app/Models/Admin.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-//use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User; //Userを引用
 use Illuminate\Notifications\Notifiable;
 
 class Admin extends User
